35c9ac865696715d3ce84310ba7f31770d05c8bc
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-29 23:51:34 -0700

    Add comments, rearrange code to start new MVC structure

--- ScoresMVC/ViewController.swift
+++ ScoresMVC/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
+    // MARK: Properties
+    var giantsScore = Score()
+    var visitorScore = Score()
+    
     // MARK: Outlets
     @IBOutlet weak var giantsScoreLabel: UILabel!
     @IBOutlet weak var visitorScoreLabel: UILabel!
@@ -17,33 +21,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var giantsScoreTextField: UITextField!
     @IBOutlet weak var visitorScoreTextField: UITextField!
 
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // Set the View Controller as the delegate
+        // Set the View Controller as the delegate for both UITextFields
         giantsScoreTextField.delegate = self
         visitorScoreTextField.delegate = self
     }
-
-    // MARK: IBActions
-    @IBAction func whoWon(_ sender: UIButton) {
-        let giantsScore: Int = Int(giantsScoreLabel.text!)!
-        let visitorScore: Int = Int(visitorScoreLabel.text!)!
-        
-        // Case 1: If Giants score is less, Visitor wins
-        if giantsScore < visitorScore {
-            winningTeamLabel.text = "Visitor"
-            // Case 2: If Giants score is greater, Giants win
-        } else if giantsScore > visitorScore {
-            winningTeamLabel.text = "Giants"
-            // Case 3: If the scores are the same, then tie
-        } else {
-            winningTeamLabel.text = "Tie"
-        }
-    }
     
-    // MARK: Methods
+    // MARK: UITextField delegate methods
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         giantsScoreTextField.resignFirstResponder()
         visitorScoreTextField.resignFirstResponder()
@@ -75,6 +63,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
                     }
                 }
             }
+        }
+    }
+
+
+    // MARK: Actions
+    
+    // IBAction for tapping "who won?" button
+    @IBAction func whoWon(_ sender: UIButton) {
+        let giantsScore: Int = Int(giantsScoreLabel.text!)!
+        let visitorScore: Int = Int(visitorScoreLabel.text!)!
+        
+        // Case 1: If Giants score is less, Visitor wins
+        if giantsScore < visitorScore {
+            winningTeamLabel.text = "Visitor"
+            // Case 2: If Giants score is greater, Giants win
+        } else if giantsScore > visitorScore {
+            winningTeamLabel.text = "Giants"
+            // Case 3: If the scores are the same, then tie
+        } else {
+            winningTeamLabel.text = "Tie"
         }
     }
 }
