3e976c44e80b98d4316f9b6df3fe849eabde7fe0
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-30 00:09:49 -0700

    Create initial version of Score model

---
+++ ScoresMVC/Score.swift
@@ -0,0 +1,22 @@
+//
+//  Score.swift
+//  ScoresMVC
+//
+//  Created by Maribel Montejano on 10/29/17.
+//  Copyright © 2017 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
+
+// MODEL
+class Score {
+    // Property
+    var score: Int = 0
+    
+    // Method
+    func setScore(input: String?) {
+        if input != nil {
+            
+        }
+    }
+}
